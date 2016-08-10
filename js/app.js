b00c408c7d74df3afcc26a21a69f49c55b814f64
David Ladowitz <david@ladowitz.com>
2016-08-09 21:51:44 -0700

    finishing directives

--- js/app.js
+++ js/app.js
@@ -14,7 +14,23 @@ weatherApp.config(function($routeProvider){
 
 
 weatherApp.directive("dailyForecast", function() {
-    return {
-        templateUrl : 'directive_templates/daily_forecast.html'
-    };
+  return {
+    templateUrl : 'directive_templates/daily_forecast.html',
+    replace: true,
+    scope: {
+      forecastObject: "="
+    },
+    link: function(scope,elem,attr){
+      utcSeconds = scope.forecastObject.dt;
+      date = new Date(0)
+      date.setUTCSeconds(utcSeconds);
+      scope.forecastObject.dt = date;
+      // console.log("Linking!...");
+      // console.log(scope.forecastObject);
+      // elem.removeAttr('class');
+    }
+  };
 });
+var utcSeconds = 1234567890;
+var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+d.setUTCSeconds(utcSeconds);
